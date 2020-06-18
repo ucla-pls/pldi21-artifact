8c82b6f695b6c5909998635024bd18d9fd3ca776
Christian Gram Kalhauge <kalhauge@cs.ucla.edu>
2020-06-18 15:30:55 +0200

    Choose total graph order

--- nix/fix/jreduce.nix
+++ nix/fix/jreduce.nix
@@ -1,8 +1,8 @@
 # Auto-generated with fixnix (version 0.0.0)
-# add gh:ucla-pls/jreduce/heads/reeval
+# add gh:ucla-pls/jreduce/heads/reeval gh:ucla-pls/reduce/heads/master
 builtins.fetchTarball {
-  name   = "jreduce_reeval_94146b";
-  url    = "https://github.com/ucla-pls/jreduce/archive/94146b67f2ebfe490963cfc66d6d2bce24b1ef6d.tar.gz";
-  sha256 = "05jhmbdg93m69snl6jm10i896i15a2lfffjzccwv1wgd6wgf3dah";
+  name   = "jreduce_reeval_f3e877";
+  url    = "https://github.com/ucla-pls/jreduce/archive/f3e87798259eefe4e2fb562b0f4960e0584a7539.tar.gz";
+  sha256 = "12jcwn8xjc4pk6yjd1zp07rm5k4ydkr7nswxzpb0q0b58mk0zzlp";
 }
 
